dbbf704677bff49843a39d4c26a4a8350e4df044
Gagan7259 <109654682+Gagan7259@users.noreply.github.com>
2022-08-11 23:31:20 +0530

    y

---
+++ HTML/image/assighment/one.js
@@ -0,0 +1,22 @@
+var num=12
+if(num%2==0){
+console.log("given number is even");
+}
+else{
+    console.log("given number odd");
+}
+
+var num=15
+if(num/7==0){
+    console.log("given number divisable by 7");
+}
+else{
+    console.log("given number not divisible by 7");
+}
+var num=14
+if(num/7===0){
+    console.log("given number divisable by 7");
+}
+else{
+    console.log("given number not divisible by 7");
+}
---
+++ HTML/js/1.js
@@ -0,0 +1,6 @@
+var a=10;
+var b=20;
+var c=30;
+var d=(b+c-a)
+console.log(d)
+console.log(typeof d)
