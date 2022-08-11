b5ffb98e1895b0b696c7160d1bac9fa3bb2841c6
Gagan7259 <109654682+Gagan7259@users.noreply.github.com>
2022-08-11 23:52:19 +0530

    g

---
+++ HTML/image/assighment/2.js
@@ -0,0 +1,21 @@
+var num=14
+if(num%7==0){
+    console.log("given number divisable by 7");
+}
+else{
+    console.log("given number not divisible by 7");
+}
+var num=15
+if(num%7==0){
+    console.log("given number divisable by 7");
+}
+else{
+    console.log("given number not divisible by 7");
+}
+var num=7
+if(num/3==0){
+    console.log("given number divisable by 7");
+}
+else{
+    console.log("given number not divisible by 7");
+}
