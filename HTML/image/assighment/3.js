89224f9547028e97b71be44531e9a449fd43ff27
Gagan7259 <109654682+Gagan7259@users.noreply.github.com>
2022-08-12 00:00:57 +0530

    j

---
+++ HTML/image/assighment/3.js
@@ -0,0 +1,14 @@
+var num=14
+if(num%3==0){
+    console.log("given number multiple of  3");
+}
+else{
+    console.log("given number not multiple of 3");
+}
+var num=12
+if(num%3==0){
+    console.log("given number multiple of  3");
+}
+else{
+    console.log("given number not multiple of 3");
+}
