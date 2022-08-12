fd869dd899344212bfdd0a7fcb5241dcc6353d32
Gagan7259 <109654682+Gagan7259@users.noreply.github.com>
2022-08-12 15:40:56 +0530

    U

---
+++ HTML/image/7js/7.js
@@ -0,0 +1,8 @@
+for(i=5;i<=15;i=i+1)
+console.log(i);
+
+for(i=15;i>=10;i=i-1)
+console.log(i);
+
+for(i=1;i<=10;i=i%2)
+console.log(i%2);
---
+++ HTML/image/assighment/6.js
@@ -0,0 +1,8 @@
+for(i=1;i<=10;i=i+1)
+console.log(i);
+
+for(i=10;i>=1;i=i-1)
+console.log(i);
+
+for(i=5;i>=15;i=i+1)
+console.log(i);
