15f762f987681d108dd64212b1e56c2aa610c5cc
Gagan7259 <109654682+Gagan7259@users.noreply.github.com>
2022-08-16 22:42:20 +0530

    u

--- HTML/array/3.js
+++ HTML/array/3.js
@@ -1,6 +1,17 @@
+let a = [10, 20, 30, 40]
+//indexing 0  1  2  3
+//how to read? using array name
+console.log(a)
+//how to read array values ? using indexing 
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[10])
 
-var arr=[10,20,30,40,50]
-for(i=0;i<=arr.length-1;i=i+1)
-{
-    console.log(i[2]);
-}
+
+let b=['manoj','gagan','prem']
+console.log(b);
+console.log(b[2]);
+console.log(b[0]);
+console.log(b[18]);
