9268bcb8086faed7713b71cb89f86a5bc010f389
Gagan7259 <109654682+Gagan7259@users.noreply.github.com>
2022-08-18 10:19:38 +0530

    y

---
+++ HTML/array/object/2.js
@@ -0,0 +1,8 @@
+let employee = {
+    id: 101,
+    Name  : "Rahul",
+    Sal: 45000
+}
+for (key in employee) {
+    console.log(employee[key])
+}
---
+++ HTML/array/object/3.js
@@ -0,0 +1,8 @@
+let user ={
+    id: 101,
+    name: "Gagan",
+    sal: 6700
+}
+for (key in user){
+    console.log(user[key])
+}
